131818f0a5c5ccb68806c1fd5b613405c55a0f51
Kaleb Petersen <kcpetersen111@gmail.com>
2022-07-12 13:13:02 -0600

    set up the commands to pull a docker image and start up the container. Next thing will be to connect the backend to the db.

---
+++ mongo-entrypoint/entrypoint.js
@@ -0,0 +1,10 @@
+let db = connect("mongodb://user:password@localhost:27017/admin");
+
+db = db.getSiblingDB("cs-forum-2022");
+
+db.createUser({
+  user: "new_user",
+  pwd: "password",
+  roles: [{ role: "readWrite", db: "cs-forum-2022" }],
+  passwordDigestor: "server",
+});
