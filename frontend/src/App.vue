be4072c552862a56243e816fd28626307a38de8f
chaz-cox <chazcheezecox@gmail.com>
2022-07-12 12:51:39 -0600

    added some login stuff so far

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <h1>Login</h1>
+    Username: <input placeholder="username..." type="text">
+    <br>
+    Password: <input placeholder="password..." type="password">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
