f27a59abb3945010e4fb3ecf86da7548322d790a
chaz-cox <chazcheezecox@gmail.com>
2022-07-14 10:56:36 -0600

    Renamed current files so they don't conflict with what is on the main branch.

---
+++ frontend/src/components/oldLogin.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <v-card class="blue lighten-5 mx-auto mt-7 pb-4 px-7" loading max-width="500">
+            <v-card-title>
+                Welcome, Please Login
+            </v-card-title>
+            <v-text-field label="username" v-model="usernameInput"></v-text-field>
+            <v-text-field label="password" v-model="passwordInput"></v-text-field>
+            <v-card-actions>
+                <v-btn @click="Login()">
+                    Submit
+                </v-btn>
+                <v-btn @click="gotoRegister()">
+                    Register
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </div>
+</template>
+
+<script lang="ts">
+    export default{
+        name: "LoginPage",
+        data: () => ({
+            usernameInput: "",
+            passwordInput: "",
+        }),
+        methods: {
+            gotoRegister(){
+                console.log("GO to Register page!");
+            },
+            Login(){
+                console.log("Login in");
+            },
+        },
+    }
+    
+</script>
+
