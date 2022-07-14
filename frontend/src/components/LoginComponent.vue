f177cdded6ea8fbb3b257bd769fd3145783adc8c
chaz-cox <chazcheezecox@gmail.com>
2022-07-14 12:51:23 -0600

    trees.

--- frontend/src/components/LoginComponent.vue
+++ frontend/src/components/LoginComponent.vue
@@ -1,21 +1,25 @@
 <template>
-    <div>
-        <v-card class="blue lighten-5 mx-auto mt-7 pb-4 px-7" loading max-width="500">
-            <v-card-title>
-                Welcome, Please Login
-            </v-card-title>
-            <v-text-field label="username" v-model="usernameInput"></v-text-field>
-            <v-text-field label="password" v-model="passwordInput"></v-text-field>
-            <v-card-actions>
-                <v-btn @click="Login()">
-                   Login 
-                </v-btn>
-                <v-btn @click="gotoRegister()">
-                    Register
-                </v-btn>
-            </v-card-actions>
-        </v-card>
-    </div>
+    <v-app>
+        <v-parallax src="https://cdn.shopify.com/s/files/1/0326/7189/articles/General_Sherman_tree_2000x.jpg?v=1627936731" height="1000">
+            <v-main>
+                <v-card class=" mx-auto mt-7 pb-4 px-7" color="rgba(255,255,255,.7)" flat max-width="500">
+                    <v-card-title style="display: block">
+                        <b>Sign in</b> 
+                    </v-card-title>
+                    <v-text-field label="username" v-model="usernameInput"></v-text-field>
+                    <v-text-field label="password" v-model="passwordInput"></v-text-field>
+                    <v-card-actions>
+                        <v-btn @click="Login()">
+                            Login 
+                        </v-btn>
+                        <v-btn @click="gotoRegister()">
+                            Register
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-main>
+        </v-parallax>
+    </v-app>
 </template>
 
 <script lang="ts">
@@ -30,6 +34,8 @@
                 console.log("GO to Register page!");
             },
             Login(){
+                this.usernameInput =  "";
+                this.passwordInput =  "";
                 console.log("Login in");
             },
         },
