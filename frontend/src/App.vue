992b7202cf0376767abdccf434cf6e277ff79c19
kendallbaker12 <kendallbaker@DESKTOP-85RL7KJ.localdomain>
2022-07-13 20:49:14 -0600

    started working on footer and registration page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,51 @@
 <template>
   <v-app>
     <v-main>
-      this doesnt blow
-      {{ page }}
-      <Calendar />
+      <v-card width="350" 
+        class="mx-auto ma-16"
+        elevation="14">
+        <v-card-title>
+          Sign up!
+        </v-card-title>
+      </v-card>
+      <v-footer fixed
+      height="100"
+      color="rgb(40%, 34%, 49%)"
+      >
+        <a id="contact-us-btn" v-on:click="buttonTest">
+          Contact us.
+        </a>
+      </v-footer>
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import Calendar from "./components/Calendar.vue";
+//import Calendar from "./components/Calendar.vue";
 
 export default Vue.extend({
   name: "App",
 
   components: {
-    Calendar,
+    //Calendar,
   },
 
   data: () => ({
     page: "test1",
     //
   }),
+  methods:{
+    buttonTest: function(){
+      console.log("it works")
+    }
+  }
 });
 </script>
+<style>
+#contact-us-btn{
+  color: black;
+  display: flex;
+  margin-left: 250px;
+}
+</style>
