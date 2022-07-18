930b1be13279f66ab673d7a16a21dff38df41a8b
kendall baker <d00262562@dmail.dixie.edu>
2022-07-18 09:04:38 -0600

    updating

--- frontend/src/components/ContactComponent.vue
+++ frontend/src/components/ContactComponent.vue
@@ -3,7 +3,13 @@
     <v-main>
       <v-card  class="mx-auto my-auto" color="brown" width="500" height="500">
         <v-card-title class="flex justify-center ">Contact Us</v-card-title>
-        <v-text-field filled></v-text-field>
+        <v-text-field class="px-7" 
+        outlined 
+        label="First Name">
+        </v-text-field>
+        <v-text-field class="px-7" 
+        outlined 
+        label="Last Name"></v-text-field>
       </v-card>
     </v-main>
   </v-app>
