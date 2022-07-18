ed79f9c8f2f5799abb7685ab5c61c28ccbfcbdbb
kendall baker <d00262562@dmail.dixie.edu>
2022-07-18 10:11:33 -0600

    added videos

--- frontend/src/components/ContactComponent.vue
+++ frontend/src/components/ContactComponent.vue
@@ -1,15 +1,26 @@
 <template>
   <v-app>
     <v-main>
+      <video autoplay muted loop id="myVideo">
+        <source src="../assets/videoblocks-aerial-clouds-forest_r61rdndsb__7977a5c90b21e53844682ceb7958572f__P360.mp4" type="video/mp4">
+      </video>
       <v-card  class="mx-auto my-auto" color="brown" width="500" height="500">
         <v-card-title class="flex justify-center ">Contact Us</v-card-title>
-        <v-text-field class="px-7" 
-        outlined 
-        label="First Name">
-        </v-text-field>
-        <v-text-field class="px-7" 
-        outlined 
-        label="Last Name"></v-text-field>
+        <v-text-field class="px-7 rounded-pill" 
+        solo-inverted
+        label="First Name"
+        ></v-text-field>
+        <v-text-field class="px-7 rounded-pill" 
+        solo-inverted
+        label="Last Name"
+        ></v-text-field>
+        <v-text-field class="px-7 rounded-pill"
+        solo-inverted
+        label="Email"></v-text-field>
+        <v-textarea class="px-7 rounded-xl"
+        solo-inverted
+        clearable
+        label="What can we help you with?"></v-textarea>
       </v-card>
     </v-main>
   </v-app>
