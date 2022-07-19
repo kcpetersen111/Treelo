b253f3e350352539df0d92e924878e619459f183
kendall baker <d00262562@dmail.dixie.edu>
2022-07-19 13:03:55 -0600

    contact being updated once again

--- frontend/src/components/ContactComponent.vue
+++ frontend/src/components/ContactComponent.vue
@@ -2,48 +2,46 @@
   <v-app>
     <video src="https://static.videezy.com/system/resources/previews/000/012/663/original/Nature_54_-_30s_-_4k_res.mp4" autoplay loop playsinline muted></video>
     <v-main>
-      <div class="d-flex">
-      <v-card class="mx-auto  mr-10 rounded-xl" width="475" height="450" style="display:inline-block">
-        <v-card-text style="width:100px">
-        test
-        </v-card-text>
-      </v-card>
-      <v-card elevation="24" class="mx-auto mr-2 rounded-xl white--text" width="475" height="450" img="https://images.pexels.com/photos/1632790/pexels-photo-1632790.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" style="display:inline-block;">
-        <v-card-title class="flex justify-center ">Contact Us</v-card-title>
-        <v-text-field id="first-name" class="px-7 my-5 mx-auto rounded-pill"
-        style="width:200px; display: inline-block;" 
-        outlined
-        background-color="brown darken-2"
-        dense
-        solo-inverted
-        label="First Name"
-        ></v-text-field>
-        <v-text-field id="last-name" class="px-7 mx-auto rounded-pill " 
-        style="width:200px; display: inline-block;"
-        background-color="brown darken-2"
-        label="Last Name"
-        solo-inverted
-        dense
-        outlined
-        ></v-text-field>
-        <v-text-field id="email-text" class="px-7 rounded-pill"
-        solo-inverted
-        background-color="brown darken-2"
-        dense
-        outlined
-        prefix="Email"
-        ></v-text-field>
-        <v-textarea id="text-area" class="px-7 rounded-pill"
-        background-color="brown darken-2"
-        dense
-        solo-inverted
-        clearable
-        label="What can we help you with?"></v-textarea>
-        <v-card-actions>
-          <v-btn id="contact-button" class=" mr-auto ml-10 mb-2 mt-n4">Contact Us!</v-btn>
-        </v-card-actions>
-      </v-card>
-      </div>
+      <!-- <v-card style="width: 70%"> -->
+        <!-- <v-card class="mx-auto mr-10 rounded-xl pa-8"> -->
+        <!--   width="475" height="450" style="display:inline-block" -->
+          <!-- <v-card-text> -->
+            <!-- style="font-size: larger; display: flex; flex-direction: column;" -->
+            <!-- <p>Here at Budget Trello!</p> -->
+          <!-- </v-card-text> -->
+        <!-- </v-card> -->
+
+        <v-card elevation="24" class="mx-auto mr-2 rounded-xl white--text" img="https://media.istockphoto.com/vectors/abstract-geometric-background-mosaic-with-triangle-patterns-brown-vector-id1223873773?k=20&m=1223873773&s=612x612&w=0&h=JWs86Y6dHrbLQGhl1Grp9j18bWiCPHclWEZDOKR4EW4=" style="display:inline-block; width: 40%;">
+          <!-- width="475" height="450" -->
+          <v-card-title class="flex justify-center text-h4">Contact Us</v-card-title>
+          <v-card-subtitle class="white--text subtitle-1">Here at budget trello!</v-card-subtitle>
+          <v-text-field id="first-name" class="px-7 my-5 mx-auto rounded-pill"
+          style="width:50%; display: inline-block; color:black;" 
+          outlined
+          dense
+          prefix="First Name"
+          color="black"
+          ></v-text-field>
+          <v-text-field id="last-name" class="px-7 mx-auto rounded-pill " 
+          style="width:200px; display: inline-block;"
+          label="Last Name"
+          dense
+          outlined
+          ></v-text-field>
+          <v-text-field id="email-text" class="px-7 rounded-pill"
+          dense
+          outlined
+          prefix="Email"
+          ></v-text-field>
+          <v-textarea id="text-area" class="px-7"
+          dense
+          clearable
+          label="What can we help you with?"></v-textarea>
+          <v-card-actions style="display:inline-block" class="mb-4">
+            <v-btn id="contact-button">Contact Us!</v-btn>
+          </v-card-actions>
+        </v-card>
+      <!-- </v-card> -->
     </v-main>
   </v-app>
 </template>
@@ -64,7 +62,8 @@ export default Vue.extend({
 #first-name,#last-name,#email-text,#text-area{
   text-align: center;
   color: black;
-
+  background-image: url(https://media.istockphoto.com/vectors/abstract-geometric-background-mosaic-with-triangle-patterns-brown-vector-id1223873773?k=20&m=1223873773&s=612x612&w=0&h=JWs86Y6dHrbLQGhl1Grp9j18bWiCPHclWEZDOKR4EW4=);
+  opacity: 0.5;
 }
 video {
   object-fit: cover;
@@ -93,5 +92,15 @@ video {
 #contact-button:hover::after {
   transform: scaleX(1);
   transform-origin: bottom left;
+}
+#contact-div-left{
+  width: 100px;
+  height: 450px;
+  background-color: black;
+  display: flex;
+  
+}
+.field-type:active {
+  border-color: green;
 }
 </style>
