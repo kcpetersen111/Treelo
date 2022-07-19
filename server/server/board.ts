4c34ac4632576123dfdef110242a4de7a2dbb888
Kaleb Petersen <kcpetersen111@gmail.com>
2022-07-19 11:12:24 -0600

    can now get all of the boards created by user: :

--- server/server/board.ts
+++ server/server/board.ts
@@ -4,10 +4,28 @@ import {User, Events, Containers, Boards} from "../persist/model";
 
 export const boardSetUp = function(app:any){
     //will need to get all boards that were created by a user 
+    app.get("/board", async (req:Request, res:Response)=>{
+        if (!req.user){
+            res.status(401).json({message:"unauthed"});
+            return;
+        }
+        let boards:[typeof Boards];
+        try {
+            boards = Boards.find({creatorId:req.user.id})
+        } catch (error) {
+            res.status(500).json({
+                message:"Internal server error, could not create board",
+                error:error,
+            });
+            return;
+        }
+
+        res.status(201).json(boards);
+    });
     //will need to update board
     //will need to delete board
-    //will need to create board
 
+    //will need to create board
     app.post("/board", async (req:Request, res:Response)=>{
         if(!req.user){
             res.status(401).json({message:"unauthed"});
