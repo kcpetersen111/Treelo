ca9940469e0eb557ed95ee8d05af8d95ed7bfa1b
chaz-cox <chazcheezecox@gmail.com>
2022-07-21 11:52:34 -0600

    almost there

--- server/server/container.ts
+++ server/server/container.ts
@@ -1,4 +1,5 @@
 import express, {Request, Response} from 'express';
+import { idText } from 'typescript';
 
 import {User, Cards, Containers, Boards} from "../persist/model";
 
@@ -6,7 +7,7 @@ export const containerSetUp = function(app:any){
 
 
     //will need to get all containers from a board
-    app.get("/container/:boardId", async (req:Request, res:Response)=>{
+    app.get("/container/board/:boardId", async (req:Request, res:Response)=>{
         const id = req.params.boardId;
         if(!req.user){
             res.status(401).json({message:"unauthed"});
@@ -15,15 +16,21 @@ export const containerSetUp = function(app:any){
         let board:typeof Boards;
         try {
             board = await Boards.findById(id);
+            if (!board){
+                res.status(404).json({
+                message:`not found`,
+            })
+            return;
+            }
+            if(board.creatorID != req.user.id){
+                res.status(403).json({"message":"you are not authorized to view that board"});
+                return;
+            }
         } catch (error) {
             res.status(500).json(error);
             return;
         }
-        if(board.ownerID != req.user.id){
-            res.status(403).json({"message":"you are not authorized to view that board"});
-            return;
-        }
-        res.status(201).json(board.cards);
+        res.status(201).json(board.container);
 
     });
     //will need to get container by id
@@ -35,55 +42,125 @@ export const containerSetUp = function(app:any){
         const containerId = req.params.containerId;
         let container;
         try{
-            container= await Containers.findOne({creatorId:req.user.id}).populate({
-                path:"events"
-            });
+            container= await Containers.findById(containerId);
+            if (!container){
+                res.status(404).json({
+                message: `not found`,
+                container_id: containerId,
+                })
+            return;
+            }
+
+            if (container.creatorID != req.user.id){
+                res.status(403).json({
+                message: `not authorized to view board`,
+            })
+            return;
+            }
+
+        
          } catch (err){
             res.status(500).json({
-                message: `error when finding board`,
+                message: `error when finding container`,
                 error: err,
             });
             return;
          }
-
-         if (!container){
-            res.status(404).json({
-                message: `not found`,
-                container_id: containerId,
-            })
-            return;
-         }
          res.status(200).json(container);
          return;
-        
+
     })
     //will need to update container by id
+    app.put("/container/:containerId", async (req:Request, res: Response)=>{
+        if (!req.user){
+            res.status(401).json({message: `unauthed`});
+            return;
+        }
+        const id = req.params.containerId;
+        let container;
+        try{
+            container = await Containers.findById(id);
+            if (!container){
+                res.status(404).json({
+                    message: `container not found`,
+                    id: id,
+                });
+                return;
+            }
+            if (container.creatorID != req.user.id){
+                res.status(403).json({
+                    message: `not authorized to change this container`,
+                });
+                return;
+            }
+        }catch(err){
+            res.status(500).json({
+                message: `failed to put`,
+                error: err,
+            });
+            return;
+        }
+        let newContainer = {
+            name: req.body.name,
+            description:req.body.description,
+            cards: req.body.cards};
+        
+        try{
+            container = await Containers.findByIdAndUpdate(id,newContainer,
+                {
+                   new: true,
+                },
+            );
+        } catch (err){
+            console.log(err);
+            res.status(500).json({
+                message: `ERROR`,
+                error: err,
+            });
+            return;
+        }
+        console.log(container);
+        res.status(201).json(container);    
+    });
     //will need to delete container by id
-    // app.delete("/container/:id", async (req:Request, res:Response)=>{
-    //     //fix later
-    //     if (!req.user){
-    //         res.status(404).json({ message: "unauthed"});
-    //         return;
-    //     }
-    //     let container;
-    //     try{
-    //         container = await Containers.findById(req.params.id);
-    //     }catch (err){
-    //         res.status(500).json({
-    //             message: `failed to delete card`,
-    //             error: err,
-    //         });
-    //         return;
-    //     }
-    //     if( card == null){
-    //         res.status(404).json({
-    //             message: `cannot find card`,
-    //             container_id: req.params.container,
-    //         });
-    //         //console.log("cannot find it");
-    //         return;
-    //     }
-    //     if( container.user_id)
+    app.delete("/board/:boardId/container/:id", async (req:Request, res:Response)=>{
+        if (!req.user){
+            res.status(401).json({ message: "unauthed"});
+            return;
+        }
+        let board;
+        let container;
+
+        try{
+            board = await Boards.findOne({
+                _id : req.params.boardId,
+                "container"
+            })
+        }
+        try{
+            container = await Containers.findById(req.params.id);
+            if (!container){
+                res.status(404).json({
+                    message: `couldnt find it`,
+                });
+                return;
+                }
+            if( container.creatorID != req.user.id){
+                res.status(403).json({
+                    message: `not authorized to delete container`,
+                })
+                return;
+            }
+        }catch (err){
+            res.status(500).json({
+                message: `failed to delete container`,
+                error: err,
+            });
+            return;
+        }
+        res.status(201).json(container);
+        return;
+    });
 
     //will need to create container on a board id
     app.post("/board/:boardID/container",async (req:Request, res:Response)=>{
@@ -92,26 +169,48 @@ export const containerSetUp = function(app:any){
             res.status(401).json({message:"unauthed"});
             return;
         }
-        let container = await Containers.create({
-            "creatorID": req.user.id,
-            "name": req.body.containerName,
-            "description": req.body.description,
-            "cards": [],
-        });
+        try{
+            let container = await Containers.create({
+                "creatorID": req.user.id,
+                "name": req.body.containerName,
+                "description": req.body.description,
+                "cards": [],
+            });
+        }catch(err){
+            res.status(500).json({
+                message: "Failed to enter in all the feilds"
+            });
+            return;
+        }
         let board;
         try {
             board = await Boards.findByIdAndUpdate(
                 id,
                 {
                     $push:{
-                        ID: container._id,
+                        container: container._id,
                     }
-                });
-            res.status(201).json(board);
+                },
+                {
+                    new: true,
+                }
+            );
+        if(!board){
+            res.status(404).json({
+                message: `Doesn't exist`
+            })
+            return;
+        }
+        if(board.creatorID != req.user.id){
+            res.status(403).json({
+                message: `not authorized to access board`,
+            })
+        }
         } catch (error) {
             res.status(500).json(error);
             return;
         }
+        res.status(201).json(board);
 
 
 
