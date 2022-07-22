cdf6b381e3a7459f103b34f23dd16f3026d33a5e
Jake Duckett <Legac3e@gmail.com>
2022-07-22 10:52:16 -0600

    Made containers and cards into their own components and set everything up so that data is passed down via props. That should make it much easier to set up the CRUD methods for each of those + make bugs less likely to creep in.

---
+++ frontend/src/components/CardComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card class="justify-center ma-2" style="display: flex">
+    <v-card-title
+      class="justify-center green--text text--darken-2 font-weight-bold"
+    >
+      hi, am a card with id {{ cardData.id }}
+    </v-card-title>
+  </v-card>
+</template>
+
+<script lang="ts">
+export default {
+  name: "CardComponent",
+  props: {
+    cardData: {
+      id: Number,
+      name: String,
+      description: String,
+    },
+  },
+  created() {
+    // console.log(this.$props.cardData);
+  },
+  methods: {
+    //
+  },
+};
+</script>
--- frontend/src/components/ContainerComponent.vue
+++ frontend/src/components/ContainerComponent.vue
@@ -1,15 +1,40 @@
 <template>
-  <ContainerComponent />
+  <v-card>
+    <v-card-title class="text-h4 purple--text font-weight-bold">
+      {{ containerData.name }}
+    </v-card-title>
+    <v-list>
+      <CardComponent
+        v-for="card in containerData.cards"
+        :key="card.cardID"
+        :cardData="card"
+      />
+    </v-list>
+  </v-card>
 </template>
 
-<script>
+<script lang="ts">
 // @ is an alias to /src
-import ContainerComponent from "@/components/ContainerComponent.vue";
+import CardComponent from "@/components/CardComponent.vue";
 
 export default {
   name: "ContainerComponent",
+  props: {
+    containerData: {
+      id: Number,
+      name: String,
+      description: String,
+      cards: Array,
+    },
+  },
   components: {
-    ContainerComponent,
+    CardComponent,
+  },
+  created() {
+    // console.log(this.$props.containerData);
+  },
+  methods: {
+    //
   },
 };
 </script>
