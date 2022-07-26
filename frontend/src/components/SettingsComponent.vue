7d2c464eabdc70ae5eb18fcd0fe7de22a1d2d378
kendall baker <d00262562@dmail.dixie.edu>
2022-07-26 11:40:16 -0600

    working on settings backgrounds

--- frontend/src/components/SettingsComponent.vue
+++ frontend/src/components/SettingsComponent.vue
@@ -1,14 +1,40 @@
 <template>
-  <v-app>
-    <video
-      src="https://vimeo.com/733378185"
-      autoplay
-      loop
-      playsinline
-      muted
-    ></video>
-    <v-card class="mx-auto mt-10" width="50%" height="50%">
+  <v-app style="background-image: url('https://t3.ftcdn.net/jpg/02/65/41/32/360_F_265413209_odsMQCc5XqnZPtm8mseWbomCEQSN945n.jpg'); background-repeat:no-repeat; background-size: cover; background-attachment: fixed;">
+    <v-card class="mx-auto mt-10 rounded-lg" width="60%" height="60%" color="black">
       <v-card width="90%" class="mx-auto mt-3" height="90%">
+        <div class="pt-4">
+          <v-text-field
+            class="mx-auto mt-5"
+            dense
+            label="Username"
+            hint="Username"
+            color="black"
+            full-width
+            v-ripple
+            style="width:40%"
+          ></v-text-field>
+          <v-text-field
+            class="mx-auto mt-4"
+            dense
+            label="Password"
+            hint="Password"
+            type="password"
+            color="black"
+            full-width
+            v-ripple
+            style="width:40%"
+          ></v-text-field>
+          <v-text-field
+            class="mx-auto mt-5"
+            dense
+            label="Email"
+            hint="Email"
+            color="black"
+            full-width
+            v-ripple
+            style="width:40%"
+          ></v-text-field>
+        </div>
         <v-btn @click="logOut()">Sign Out</v-btn>
       </v-card>
     </v-card>
