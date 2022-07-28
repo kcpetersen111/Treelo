7977487f06393cb6fe5db67cc63b6b5f1a94758e
kendall baker <d00262562@dmail.dixie.edu>
2022-07-28 11:25:34 -0600

    added hover effects to buttons and changed color

--- frontend/src/components/BoardComponent.vue
+++ frontend/src/components/BoardComponent.vue
@@ -15,10 +15,12 @@
     </v-row>
     <div style="position: fixed; right: 1%; bottom: 10%">
       <v-btn @click="showContainer = true"
+        class="tree-buttons"
+        color="blue-grey lighten-3"
         >+<v-icon>mdi-source-branch</v-icon></v-btn
       ><!--package-variant-plus-->
       <!-- delete board -->
-      <v-btn @click="overlay = !overlay">-<v-icon>mdi-axe</v-icon>
+      <v-btn @click="overlay = !overlay" class="tree-buttons" color="blue-grey lighten-3">-<v-icon>mdi-axe</v-icon>
         <v-overlay :value="overlay">
         <v-card width="400" height="400" class="mx-auto white--text" style="padding-top: 40%;background-image:url('https://images.unsplash.com/photo-1613858636109-354616495373?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80');background-size: cover;">
           <div>
